dd9620e8f3c93bd048c6ae8138cfbe91bfc2eb28
Tawnee <allprt89@gmail.com>
2023-05-03 17:43:23 -0600

    update api

---
+++ pages/api/weather.ts
@@ -0,0 +1,25 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type Data = {
+  name: string;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  if (!process.env.WEATHER_API_KEY) {
+    return res.status(500).end("Missing API key");
+  }
+  const url = new URL("https://api.openweathermap.org/data/2.5/weather");
+  url.searchParams.set("lat", "44.526340");
+  url.searchParams.set("lon", "-109.056534");
+  url.searchParams.set("appid", process.env.WEATHER_API_KEY);
+  url.searchParams.set("units", "imperial");
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  res.status(200).json(data);
+}
